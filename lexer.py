e13a4636b0da9324ed875f6e6ee158f7dce19d43
qwercik <erykandrzejewski@gmail.com>
2019-10-28 15:32:38 +0100

    Refactoring

--- lexer.py
+++ lexer.py
@@ -1,3 +1,8 @@
+import re
+
+class IncorrectTokenError(Exception):
+    pass
+
 tokensTypes = {
     'constant': r'^[a-e]$',
     'variable': r'^[A-Z]$',
@@ -20,3 +25,27 @@ tokensCategories = {
     'binary_operator': ['conjunction', 'disjunction', 'implication', 'equivalence', 'exclusionary_alternative'],
     'quantifier': ['universal_quantifier', 'existential_quantifier']
 }
+
+def tokenizeRpn(rpnFormula):
+    tokens = []
+
+    symbols = rpnFormula.split()
+    for symbol in symbols:
+        for tokenType, regex in tokensTypes.items():
+            matchResult = re.findall(regex, symbol)
+            if matchResult:
+                tokens.append({
+                    'type': tokenType,
+                    'category': tokenCategory(tokenType),
+                    'data': matchResult[0]
+                })
+                break
+        else:
+            raise IncorrectTokenError(token)
+
+    return tokens
+
+def tokenCategory(tokenType):
+    for tokenCategory, types in tokensCategories.items():
+        if tokenType in types:
+            return tokenCategory
--- r2i.py
+++ r2i.py
@@ -3,36 +3,16 @@
 import re
 import sys
 import string
-from tokens import tokensTypes, tokensCategories
-
-class IncorrectTokenError(Exception):
-    pass
-
-def parseRpn(rpnFormula):
-    parsedOutput = []
-
-    tokens = rpnFormula.split()
-    for token in tokens:
-        for tokenType, regex in tokensTypes.items():
-            matchResult = re.findall(regex, token)
-            if matchResult:
-                parsedOutput.append({ 'type': tokenType, 'data': matchResult[0] })
-                break
-        else:
-            raise IncorrectTokenError(token)
-
-    return parsedOutput
+import lexer
 
 def translateRpnToInfix(rpnFormula):
+    tokens = lexer.tokenizeRpn(rpnFormula)
+
     stack = []
     infixFormula = ''
 
-    parsedFormula = parseRpn(rpnFormula)
-    for symbol in parsedFormula:
-        for tokenCategory, types in tokensCategories.items():
-            if symbol['type'] in types:
-                print(symbol['data'], symbol['type'], tokenCategory)
-                break
+    for token in tokens:
+        print(token)
 
     return infixFormula
 
