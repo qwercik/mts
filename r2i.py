79ef1c17054fbc000a2465b156c788c771696f4f
qwercik <erykandrzejewski@gmail.com>
2019-10-28 16:10:31 +0100

    First part of code translating rpn to infix

--- r2i.py
+++ r2i.py
@@ -5,15 +5,44 @@ import sys
 import string
 import lexer
 
-def translateRpnToInfix(rpnFormula):
-    tokens = lexer.tokenizeRpn(rpnFormula)
+def popFew(inputList, count):
+    data = inputList[-count:]
+    inputList = inputList[:-count]
+    return (inputList, data)
 
+def translateRpnToInfix(rpnFormula):
     stack = []
     infixFormula = ''
 
+    tokens = lexer.tokenizeRpn(rpnFormula)
     for token in tokens:
-        print(token)
+        if token['category'] == 'value':
+            stack.append(token)
+        elif token['category'] == 'expression_with_parenthesis':
+            symbol, arity = token['data']
+            stack, arguments = popFew(stack, int(arity))
+            
+            textOutput = symbol + '(' + ', '.join(map(lambda arg: arg['data'], arguments)) + ')'
+            stack.append({ 'type': 'text', 'category': 'text', 'data': textOutput })
+        elif token['category'] == 'unary_operator':
+            symbol = token['data']
+            argument = stack.pop()
 
+            textOutput = symbol + ' ' + argument['data']
+            stack.append({ 'type': 'text', 'category': 'text', 'data': textOutput })
+        elif token['category'] == 'binary_operator':
+            symbol = token['data']
+            stack, arguments = popFew(stack, 2)
+
+            textOutput = arguments[0]['data'] + ' ' + symbol + ' ' + arguments[1]['data']
+            stack.append({ 'type': 'text', 'category': 'text', 'data': textOutput })
+        elif token['category'] == 'quantifier':
+            symbol = token['data']
+            stack, arguments = popFew(stack, 2)
+
+            textOutput = symbol + ' ' + arguments[0]['data'] + ' ' + arguments[1]['data']
+            stack.append({ 'type': 'text', 'category': 'text', 'data': textOutput })
+        
     return infixFormula
 
 def main():
