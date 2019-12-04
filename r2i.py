9ac338f9e8221c0d8e85f41db00692defea4009a
qwercik <erykandrzejewski@gmail.com>
2019-12-04 16:51:09 +0100

    Support for internationalization

--- r2i.py
+++ r2i.py
@@ -3,14 +3,31 @@
 import re
 import sys
 import string
+import json
 import lexer
 import debug
 import exitcodes
 import translator
 
 DEVELOPER_URL = 'https://github.com/qwercik/r2i'
+CONFIG_FILE = 'config.json'
+TRANSLATION_FILE = 'lang.json'
+
+def readTranslations(lang):
+    translations = dict(map(lambda x: (x[0], x[1][lang]), json.loads(open(TRANSLATION_FILE).read()).items()))
+
+    return translations
+
 
 def main():
+    translations = None
+    try:
+        lang = json.loads(open(CONFIG_FILE).read())['lang']
+        translations = readTranslations(lang)
+    except:
+        debug.error('You have not typed a valid app language in config.json')
+
+
     for line in sys.stdin:
         rpnFormula = line.strip()
 
@@ -18,13 +35,13 @@ def main():
             infixFormula = translator.translateRpnToInfix(rpnFormula)
             print(infixFormula)
         except lexer.IncorrectSymbolError as symbol:
-            debug.error('Incorrect symbol: ' + str(symbol), exitcodes.EXIT_INCORRECT_SYMBOL)
+            debug.error(translations['incorrect_symbol'] + ': ' + str(symbol), exitcodes.EXIT_INCORRECT_SYMBOL)
         except translator.NestedPredicateError as formula:
-            debug.error('You nested a predicate in formula: ' + str(formula), exitcodes.EXIT_INCORRECT_FORMULA)
+            debug.error(translations['predicate_nested'] + ': ' + str(formula), exitcodes.EXIT_INCORRECT_FORMULA)
         except translator.NotEnoughArguments as formula:
-            debug.error('You have not given valid amount of arguments in function/predicate: ' + str(formula), exitcodes.EXIT_INCORRECT_FORMULA)
+            debug.error(translations['not_valid_arity'] + ': ' + str(formula), exitcodes.EXIT_INCORRECT_FORMULA)
         except translator.IncorrectFormulaError as formula:
-            debug.error('Incorrect formula: ' + str(formula), exitcodes.EXIT_INCORRECT_FORMULA)
+            debug.error(translations['unknown_error'] + ': ' + str(formula), exitcodes.EXIT_INCORRECT_FORMULA)
         except:
             debug.error(f'Unknown error. Report the developer ({DEVELOPER_URL})', exitcodes.EXIT_UNKNOWN_ERROR)
 
